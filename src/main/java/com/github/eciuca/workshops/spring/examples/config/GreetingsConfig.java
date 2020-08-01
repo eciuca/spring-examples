c42946fb14344adf0de5675dc38856584a7cfe1d
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2020-08-01 22:40:11 +0300

    Java Config with Qualifiers

--- src/main/java/com/github/eciuca/workshops/spring/examples/config/GreetingsConfig.java
+++ src/main/java/com/github/eciuca/workshops/spring/examples/config/GreetingsConfig.java
@@ -2,6 +2,7 @@ package com.github.eciuca.workshops.spring.examples.config;
 
 import com.github.eciuca.workshops.spring.examples.noioc.GreetingManager;
 import com.github.eciuca.workshops.spring.examples.noioc.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,16 +10,19 @@ import org.springframework.context.annotation.Configuration;
 public class GreetingsConfig {
 
     @Bean
-    public GreetingManager greetingManager() {
-        return new GreetingManager(englishGreetingService(), romanianGreetingService());
+    public GreetingManager greetingManager(@Qualifier("English") GreetingService service,
+                                           @Qualifier("Romanian") GreetingService service2) {
+        return new GreetingManager(service, service2);
     }
 
     @Bean
+    @Qualifier("English")
     public GreetingService englishGreetingService() {
         return new GreetingService("Hello");
     }
 
     @Bean
+    @Qualifier("Romanian")
     public GreetingService romanianGreetingService() {
         return new GreetingService("Salut");
     }
