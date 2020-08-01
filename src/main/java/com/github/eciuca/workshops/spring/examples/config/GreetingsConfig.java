b9ad7bdc3206d14e7c840e1283406c1b061f47ae
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2020-08-01 22:12:27 +0300

    Lifecycle methods with @Bean annotation

--- src/main/java/com/github/eciuca/workshops/spring/examples/config/GreetingsConfig.java
+++ src/main/java/com/github/eciuca/workshops/spring/examples/config/GreetingsConfig.java
@@ -1,5 +1,6 @@
 package com.github.eciuca.workshops.spring.examples.config;
 
+import com.github.eciuca.workshops.spring.examples.beans.MyBean;
 import com.github.eciuca.workshops.spring.examples.noioc.GreetingManager;
 import com.github.eciuca.workshops.spring.examples.noioc.GreetingService;
 import org.springframework.context.annotation.Bean;
@@ -21,5 +22,12 @@ public class GreetingsConfig {
     @Bean
     public GreetingService romanianGreetingService() {
         return new GreetingService("Salut");
+    }
+
+    @Bean(name = "mybean", initMethod = "defaultIfEmpty", destroyMethod = "destroy")
+    public MyBean mybean() {
+        MyBean myBean = new MyBean();
+        myBean.setName("world");
+        return myBean;
     }
 }
