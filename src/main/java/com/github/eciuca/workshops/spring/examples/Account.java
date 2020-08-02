c893f963c0501b64e217230d977de877c1a04ddf
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2020-08-02 10:19:57 +0300

    Create Account model

---
+++ src/main/java/com/github/eciuca/workshops/spring/examples/Account.java
@@ -0,0 +1,32 @@
+package com.github.eciuca.workshops.spring.examples;
+
+public class Account {
+
+    private String iban;
+    private String holder;
+    private double balance;
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
--- src/main/java/com/github/eciuca/workshops/spring/examples/Application.java
+++ src/main/java/com/github/eciuca/workshops/spring/examples/Application.java
@@ -4,6 +4,17 @@ import com.github.eciuca.workshops.spring.examples.config.AppConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+/**
+ * Bank Account Management Application
+ *
+ * Functionalitati:
+ *  - creare cont
+ *  - afisare lista conturi
+ *  - salvare lista conturi (in memorie / pe disk)
+ *
+ *
+ *
+ */
 public class Application {
 
     public static void main(String[] args) {
