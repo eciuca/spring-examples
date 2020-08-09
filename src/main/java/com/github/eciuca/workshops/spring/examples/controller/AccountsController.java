7e702991b56983904407ad37eac8fbb3579dc524
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2020-08-09 10:14:01 +0300

    Display data in html using thymeleaf and the Model interface

--- src/main/java/com/github/eciuca/workshops/spring/examples/controller/AccountsController.java
+++ src/main/java/com/github/eciuca/workshops/spring/examples/controller/AccountsController.java
@@ -1,16 +1,29 @@
 package com.github.eciuca.workshops.spring.examples.controller;
 
+import com.github.eciuca.workshops.spring.examples.model.Account;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AccountsController {
 
 //    @RequestMapping(path = "/accounts", method = RequestMethod.GET)
-    @GetMapping({"/conturi", "/accounts"}) //matches agains the url from the browser address bar:
+    //matches agains the url from the browser address bar:
     // http://localhost:8080/conturi
     // http://localhost:8080/accounts
-    public String getAccountsList(){
+    @GetMapping({"/conturi", "/accounts"})
+    public String getAccountsList(Model model) {
+
+        Account account = new Account();
+        account.setIban("RO123");
+        account.setHolder("Emanuel");
+        account.setBalance(100);
+
+        model.addAttribute("dummyAccountName", "My dummy account");
+        model.addAttribute("myAccount", account);
+
+
         return "accounts-list"; //the name of the html template
     }
 }
