68d9a066e1c92a99dad471f8e2771786c99e9ec5
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2020-08-09 11:43:29 +0300

    Implement Edit/Update functionality

--- src/main/java/com/github/eciuca/workshops/spring/examples/controller/AccountsController.java
+++ src/main/java/com/github/eciuca/workshops/spring/examples/controller/AccountsController.java
@@ -1,15 +1,18 @@
 package com.github.eciuca.workshops.spring.examples.controller;
 
 import com.github.eciuca.workshops.spring.examples.model.Account;
+import com.github.eciuca.workshops.spring.examples.repository.AccountRepository;
 import com.github.eciuca.workshops.spring.examples.service.AccountService;
 import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Controller
@@ -17,6 +20,10 @@ public class AccountsController {
 
     @Autowired
     private AccountService service;
+
+    @Autowired
+    private AccountRepository repository;
+
 
 //    @RequestMapping(path = "/accounts", method = RequestMethod.GET)
     //matches agains the url from the browser address bar:
@@ -66,6 +73,25 @@ public class AccountsController {
     @GetMapping("/accounts/delete/{id}")
     public String deleteAccount(@PathVariable("id") Long id) {
         service.deleteById(id);
+
+        return "redirect:/accounts";
+    }
+
+    @GetMapping("/accounts/edit/{id}")
+    public String showAccountsEditPage(@PathVariable("id") Long id, Model model) {
+        Account account = repository.findById(id).orElseThrow(() -> new EntityNotFoundException());
+
+        model.addAttribute("account", account);
+
+        return "accounts-edit";
+    }
+
+    @Transactional
+    @PostMapping("/accounts/update/{id}")
+    public String saveAccountChanges(@PathVariable("id") Long id, Account account) {
+        account.setId(id);
+
+        repository.save(account);
 
         return "redirect:/accounts";
     }
